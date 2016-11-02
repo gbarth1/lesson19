805aa3beb4d41d9a5a180093dd185850551a452e
gbarth1 <grace.m.barth@gmail.com>
2016-11-02 14:11:29 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,47 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        //ARRAY OBJECTS
+
+	    //Circle cir[]=new Circle[500];
+        //for(int j=0;j<500;j++)
+        //cir[j]=new Circle(10)---500 circles with the radius of 10(different radius= Math.random)
+
+        //if(n[23]==n[k+1])---numbers(int, double)
+        //if(s[3+d].equals("hermit"))---string arrays
+        //if(s[3+d].compareTo("hermit")>0)---compareTo(taking values and figuring out how far away they are from eachother
+        //if(BankAccount[1].equals(BankAccount[2]))---objects
+
+        //double mxz[];
+        //mxz[3]=19.1---ERROR
+        //never set amount of values
+
+        //int[] jawn={1,2,3,4,5};
+        //int[] sec;
+        //sec=jawn;(anything done to sec from here on out will change jawn)(if you change jawn it will change sec)
+        //sec[2]=99;
+        //SOP(jawn[2]);
+        //output---99
+
+        //int x[], y;---only x is an array
+
+        //int my Array[]=new int[500];
+        //myArray=null;---clearing an array
+        //myArray[45]=2003---null pointer exception
+
+        //System.arraycopy
+        //System.arraycopy(thefromarray, from index, thetoarray, toindex, howmany)
+
+        //physical size=   new int[30]
+        //logical size=    int max=5;
+
+        //import java.util.*;---to use array class
+        //Sort---sort array in ascending order---Arrays.sort(b);
+
+        //enhanced for-loops
+        //for(int varName: x)---goes through ever element in your array
+    }
+}
